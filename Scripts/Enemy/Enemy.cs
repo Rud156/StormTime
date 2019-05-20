40afcc696626add719ea5a6a74be13f7d16010c0
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-05-20 09:47:45 +0530

    Added Vecttor Helpers and Basic Enemy Movement

--- Scripts/Enemy/Enemy.cs
+++ Scripts/Enemy/Enemy.cs
@@ -1,21 +1,49 @@
 using Godot;
+using StormTime.Utils;
 using System;
 
-public class Enemy : KinematicBody2D
+public abstract class Enemy : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    // Enemy State
+    [Export] public float explorationRadius;
+    [Export] public float idleTime;
 
-    // Called when the node enters the scene tree for the first time.
+    // Target Player Stats
+    [Export] public float playerTargetDistance;
+    [Export] public float maxPlayerFollowDistance;
+    [Export] public float movementSpeed;
+
+    protected enum EnemyState
+    {
+        Idling,
+        Wandering,
+        Targeting,
+        Attacking,
+        Dead,
+    }
+
+    protected EnemyState _enemyState;
+
+    private Vector2 _startPosition;
+    private Vector2 _targetPosition;
+    private Vector2 _positionBeforePlayerFollow;
+
     public override void _Ready()
+    {
+        SetEnemyState(EnemyState.Idling);
+
+        _startPosition = GetPosition();
+        _positionBeforePlayerFollow = _startPosition;
+
+        _targetPosition = GetNewPositionForIdling();
+    }
+
+    public override void _Process(float delta)
     {
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    protected Vector2 GetNewPositionForIdling() => VectorHelpers.Random2D() * explorationRadius;
+
+    protected void SetEnemyState(EnemyState enemyState) => _enemyState = enemyState;
 }
---
+++ Scripts/Utils/VectorHelpers.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+namespace StormTime.Utils
+{
+    public class VectorHelpers : Node2D
+    {
+        public static Vector2 Random2D()
+        {
+            float randomValue = (float)GD.RandRange(0, 1);
+            return Vector2FromAngle(randomValue * Mathf.Tau);
+        }
+
+        public static Vector2 Vector2FromAngle(float angle, float length = 1)
+        {
+            Vector2 vector = new Vector2();
+            vector.x = length * Mathf.Cos(angle);
+            vector.y = length * Mathf.Sin(angle);
+
+            return vector;
+        }
+    }
+}
