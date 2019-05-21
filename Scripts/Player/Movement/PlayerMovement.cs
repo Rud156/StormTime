a2917bb5f3cb192f283a4c024ebc14cde4e49fea
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-05-21 14:24:27 +0530

    Removed Moving Background and Fixed Player Movement

--- Scripts/Player/Movement/PlayerMovement.cs
+++ Scripts/Player/Movement/PlayerMovement.cs
@@ -11,24 +11,11 @@ namespace StormTime.Player.Movement
 
         private Vector2 _movement;
 
-        public override void _Ready()
-        {
-            base._Ready();
+        public override void _Ready() => _movement = new Vector2();
 
-            _movement = new Vector2();
-        }
+        public override void _Process(float delta) => RotatePlayer(delta);
 
-        public override void _Process(float delta)
-        {
-            base._Process(delta);
-            RotatePlayer(delta);
-        }
-
-        public override void _PhysicsProcess(float delta)
-        {
-            base._PhysicsProcess(delta);
-            MovePlayer(delta);
-        }
+        public override void _PhysicsProcess(float delta) => MovePlayer(delta);
 
         private void RotatePlayer(float delta)
         {
@@ -39,20 +26,20 @@ namespace StormTime.Player.Movement
         private void MovePlayer(float delta)
         {
             if (Input.IsActionPressed(SceneControls.Left))
-                _movement.x = -movementSpeed * delta;
+                _movement.x = -movementSpeed;
             else if (Input.IsActionPressed(SceneControls.Right))
-                _movement.x = movementSpeed * delta;
+                _movement.x = movementSpeed;
             else
                 _movement.x = 0;
 
             if (Input.IsActionPressed(SceneControls.Up))
-                _movement.y = -movementSpeed * delta;
+                _movement.y = -movementSpeed;
             else if (Input.IsActionPressed(SceneControls.Down))
-                _movement.y = movementSpeed * delta;
+                _movement.y = movementSpeed;
             else
                 _movement.y = 0;
 
-            _movement = MoveAndSlide(_movement);
+            MoveAndSlide(_movement);
         }
     }
 }
