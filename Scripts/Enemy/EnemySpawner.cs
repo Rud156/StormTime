85e53decccc68b7a1948c4662d65e2cdabef8071
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-05-28 12:40:10 +0530

    More Scene Changes

--- Scripts/Enemy/EnemySpawner.cs
+++ Scripts/Enemy/EnemySpawner.cs
@@ -1,21 +1,62 @@
 using Godot;
-using System;
+using System.Collections.Generic;
+using StormTime.Enemy;
 
 public class EnemySpawner : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] public PackedScene enemyGroupPrefab;
+    [Export] public int maxEnemyGroupsToSpawn;
+    [Export] public Godot.Collections.Array<NodePath> worldSpawnNodePaths;
 
-    // Called when the node enters the scene tree for the first time.
+    private List<EnemySpawnPoint> _worldSpawnPoints;
+    private List<EnemySpawnPoint> _availableWorldSpawnPoints;
+
+    private int _currentEnemyGroups;
+    private bool _startSpawn;
+    
     public override void _Ready()
     {
-        
+        _worldSpawnPoints = new List<EnemySpawnPoint>();
+        _availableWorldSpawnPoints = new List<EnemySpawnPoint>();
+        _currentEnemyGroups = 0;
+
+        foreach (NodePath worldSpawnNodePath in worldSpawnNodePaths)
+        {
+            _worldSpawnPoints.Add(GetNode<EnemySpawnPoint>(worldSpawnNodePath));
+        }
+
+        _availableWorldSpawnPoints.AddRange(_worldSpawnPoints);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float delta)
+    {
+        if (!_startSpawn)
+        {
+            return;
+        }
+
+        if (_currentEnemyGroups >= maxEnemyGroupsToSpawn)
+        {
+            _startSpawn = false;
+            _currentEnemyGroups = 0;
+            return;
+        }
+
+        SpawnEnemyGroup();
+        _currentEnemyGroups += 1;
+    }
+
+    private void SpawnEnemyGroup()
+    {
+        int randomIndex = (int) (GD.Randi() % _availableWorldSpawnPoints.Count);
+        EnemySpawnPoint spawnNode = _availableWorldSpawnPoints[randomIndex];
+        Vector2 spawnPosition = spawnNode.GetGlobalPosition();
+
+        EnemyGroup enemyGroupInstance = (EnemyGroup) enemyGroupPrefab.Instance();
+        enemyGroupInstance.ActivateEnemySpawning(spawnNode.GetEnemyDangerLevel());
+        enemyGroupInstance.SetPosition(spawnPosition);
+
+        AddChild(enemyGroupInstance);
+        _availableWorldSpawnPoints.RemoveAt(randomIndex);
+    }
 }
