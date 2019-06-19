8a8ca28ffe59bd56fe4ff7b4770bbc8b66aae603
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-06-19 09:55:28 +0530

    Added Flames Animation

---
+++ Scripts/Player/UIDisplay/PlayerSoulsAnimationController.cs
@@ -0,0 +1,25 @@
+using Godot;
+using StormTime.Utils;
+
+namespace StormTime.Player.UIDisplay
+{
+    public class PlayerSoulsAnimationController : AnimationTree
+    {
+        private const string PlaybackPath = "parameters/playback";
+        private const string IdleAnimationPath = "Idle";
+        private const string ActionAnimationPath = "Action";
+
+        private AnimationNodeStateMachinePlayback _stateMachine;
+
+        public override void _Ready() =>
+            _stateMachine = (AnimationNodeStateMachinePlayback)Get(PlaybackPath);
+
+        #region External Functions
+
+        public void PlayIdleAnimation() => _stateMachine.Travel(IdleAnimationPath);
+
+        public void PlayActionAnimation() => _stateMachine.Travel(ActionAnimationPath);
+
+        #endregion
+    }
+}
