205008bb5e5e71b72d11fa2e95dcaaa82e2f48eb
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-03 23:50:29 -0600

    Testing Circle World Fill Logic

--- Scripts/Enemy/Boss/CircleWorldFill.cs
+++ Scripts/Enemy/Boss/CircleWorldFill.cs
@@ -4,11 +4,40 @@ namespace StormTime.Enemy.Boss
 {
     public class CircleWorldFill : BossBaseAttack
     {
-        [Export] public int circleWorldFillCount;
+        // Attack Affectors
+        [Export] public int burstCircleCount;
+        [Export] public float timeBetweenBursts;
+
+        // Display Objects
+        [Export] public PackedScene projectilePackedScene;
+        [Export] public NodePath launchPointNodePath;
+
+        private Node2D _launchPoint;
+        private float _currentTimeBetweenBurst;
+
+        public override void _Ready()
+        {
+            _launchPoint = GetNode<Node2D>(launchPointNodePath);
+        }
+
+        #region Overridden Parent
 
         public override bool UpdateAttack(float delta)
         {
             return base.UpdateAttack(delta);
         }
+
+        public override void LaunchAttack()
+        {
+            base.LaunchAttack();
+        }
+
+        #endregion
+
+        #region Utility Functions
+
+
+
+        #endregion
     }
 }
