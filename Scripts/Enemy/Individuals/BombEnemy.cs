462ac036b5682f28dfea955523d7926fe996000b
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-24 22:44:59 -0600

    Added Bomb Enemy

--- Scripts/Enemy/Individuals/BombEnemy.cs
+++ Scripts/Enemy/Individuals/BombEnemy.cs
@@ -1,33 +1,70 @@
 using Godot;
+using StormTime.Player.Data;
+using StormTime.Utils;
+using StormTime.Weapon;
 
 namespace StormTime.Enemy.Individuals
 {
     public class BombEnemy : Enemy
     {
-        [Export] public float shootWaitDelay;
+        [Export] public float timeBetweenShots;
         [Export] public PackedScene bombPrefab;
-        [Export] public float playerTargetRotationRate;
+        [Export] public float rotationRate;
+
+        private float _targetRotation;
+        private float _currentAttackTimer;
+
 
         #region Overridden Parent
 
         protected override void LaunchAttack()
         {
-
-        }
-
-        protected override void EndAttack()
-        {
-
+            base.LaunchAttack();
+            _currentAttackTimer = timeBetweenShots;
         }
 
         protected override void UpdateAttacking(float delta)
         {
+            base.UpdateAttacking(delta);
 
+            if (_currentAttackTimer > 0)
+            {
+                OrientEnemyToPlayer(delta);
+                _currentAttackTimer -= delta;
+            }
+            else if (_currentAttackTimer <= 0)
+            {
+                EnemyLaunchSingleShotAttack();
+                _currentAttackTimer = timeBetweenShots;
+            }
         }
 
         protected override void EnemyLaunchSingleShotAttack()
         {
+            BombBullet bombBulletInstance = (BombBullet)bombPrefab.Instance();
+            GetParent().AddChild(bombBulletInstance);
 
+            Node2D launchPoint = _launchPoints[0];
+            bombBulletInstance.SetGlobalPosition(launchPoint.GetGlobalPosition());
+
+            float rotation = _rotationNode.GetGlobalRotation();
+            float xVelocity = Mathf.Cos(rotation);
+            float yVelocity = Mathf.Sin(rotation);
+            Vector2 launchVector = new Vector2(xVelocity, yVelocity);
+            bombBulletInstance.LaunchBullet(launchVector.Normalized());
+        }
+
+        protected override void OrientEnemyToPlayer(float delta)
+        {
+            Vector2 currentPosition = GetGlobalPosition();
+            _targetRotation = -Mathf.Rad2Deg(Mathf.Atan2(
+                currentPosition.x - PlayerVariables.LastPlayerPosition.x,
+                currentPosition.y - PlayerVariables.LastPlayerPosition.y
+            )) - 90;
+
+
+            float currentRotation = ExtensionFunctions.LerpAngleDeg(GetGlobalRotationDegrees(), _targetRotation, rotationRate * delta);
+            SetGlobalRotation(currentRotation);
         }
 
         #endregion
--- Scripts/Weapon/BombBullet.cs
+++ Scripts/Weapon/BombBullet.cs
@@ -1,21 +1,8 @@
 using Godot;
-using System;
 
-public class BombBullet : Node
+namespace StormTime.Weapon
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public class BombBullet : EnemyBullet
     {
-        
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
